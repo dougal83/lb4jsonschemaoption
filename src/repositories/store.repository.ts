2bd37181225d4955e5f93dcc6ab7e50df5b4359e
Felix <>
2020-01-28 11:18:29 +0100

    add belong to repository

--- src/repositories/store.repository.ts
+++ src/repositories/store.repository.ts
@@ -1,16 +1,23 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Store, StoreRelations} from '../models';
-import {MydbDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import { DefaultCrudRepository, BelongsToAccessor, repository } from '@loopback/repository';
+import { Store, StoreRelations, Country, User } from '../models';
+import { MydbDataSource } from '../datasources';
+import { inject, Getter } from '@loopback/core';
+import { UserRepository } from './user.repository';
+import { CountryRepository } from '.';
 
 export class StoreRepository extends DefaultCrudRepository<
   Store,
   typeof Store.prototype.id,
   StoreRelations
-> {
+  > {
+  public readonly country: BelongsToAccessor<Country, string>;
   constructor(
     @inject('datasources.mydb') dataSource: MydbDataSource,
+    @repository.getter('CountryRepository')
+    countryRepositoryGetter: Getter<CountryRepository>,
   ) {
     super(Store, dataSource);
+    this.country = this.createBelongsToAccessorFor('country', countryRepositoryGetter)
+    this.registerInclusionResolver('country', this.country.inclusionResolver)
   }
 }

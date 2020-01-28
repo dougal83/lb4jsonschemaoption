8e4d330bf5ccc15fee9f380a87826e5c7dd6787d
Douglas McConnachie <dougal83+git@gmail.com>
2020-01-28 19:42:11 +0000

    add controllers to test includes

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from './ping.controller';
 export * from './store.controller';
 export * from './store-province.controller';
+export * from './user.controller';
+export * from './country.controller';
